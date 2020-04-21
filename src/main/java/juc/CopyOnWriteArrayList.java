fb90e4a093f626a68b863d4e234be6102f14c325
wis-w <1044957202@qq.com>
2020-04-21 20:46:22 +0800

    [2020-04-21]集合不安全问题

--- src/main/java/juc/CopyOnWriteArrayList.java
+++ src/main/java/juc/CopyOnWriteArrayList.java
@@ -1,17 +1,33 @@
 package juc;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
+import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * @author wyg_edu
  * @version 1.0
  * @date 2020/4/21 8:25
+ * 集合不安全类问题
  */
 public class CopyOnWriteArrayList {
 
     public static void main(String[] args) {
+        setNotSafe();
+    }
+
+    private static void setNotSafe() {
+        // Set<String> set = new HashSet<String>();  // 线程不安全
+        // Set<String> set = Collections.synchronizedSet(new HashSet<String>()); // 线程安全
+        Set<String> set = new CopyOnWriteArraySet<String>(); //线程安全 读写分离 底层是CopyOnWriteArrayList
+        for (int i = 1; i < 100; i++) {
+            new Thread(() -> {
+                set.add(UUID.randomUUID().toString().substring(0, 8));
+                System.out.println(set);
+            }, "Thread:" + i).start();
+        }
+    }
+
+    private static void listNotSafe() {
         List<String> list = new java.util.concurrent.CopyOnWriteArrayList<String>();
 //        List<String> list = new ArrayList<>();
         for (int i = 0; i < 100; i++) {
