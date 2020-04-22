62187fb2f6dcf1d30ffbb1e9fccd0160c081eb1b
wis-w <1044957202@qq.com>
2020-04-22 20:46:48 +0800

    【2020-04-20】可重复锁(递归锁机制)

--- src/main/java/juc/CopyOnWriteArrayList.java
+++ src/main/java/juc/CopyOnWriteArrayList.java
@@ -1,52 +1,54 @@
 package juc;
 
-import java.util.*;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArraySet;
 
 /**
  * @author wyg_edu
  * @version 1.0
- * @date 2020/4/21 8:25
- * 集合不安全类问题
+ * @date 2020/4/21 8:25 集合不安全类问题1
  */
 public class CopyOnWriteArrayList {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 //        Map<String,Object> map = new HashMap();
-        Map<String, Object> map =new ConcurrentHashMap <String, Object>();
+		Map<String, Object> map = new ConcurrentHashMap<String, Object>();
 
-        for (int i = 0; i < 100; i++) {
-            new Thread(()->{
-                map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0, 8));
-                System.out.println(map);
-            }).start();
+		for (int i = 0; i < 100; i++) {
+			new Thread(() -> {
+				map.put(Thread.currentThread().getName(), UUID.randomUUID().toString().substring(0, 8));
+				System.out.println(map);
+			}).start();
 
-        }
-    }
+		}
+	}
 
-    private static void setNotSafe() {
-        // Set<String> set = new HashSet<String>();  // 线程不安全
-        // Set<String> set = Collections.synchronizedSet(new HashSet<String>()); // 线程安全
-        Set<String> set = new CopyOnWriteArraySet<String>(); //线程安全 读写分离 底层是CopyOnWriteArrayList
-        for (int i = 1; i < 100; i++) {
-            new Thread(() -> {
-                set.add(UUID.randomUUID().toString().substring(0, 8));
-                System.out.println(set);
-            }, "Thread:" + i).start();
-        }
-    }
+	private static void setNotSafe() {
+		// Set<String> set = new HashSet<String>(); // 线程不安全
+		// Set<String> set = Collections.synchronizedSet(new HashSet<String>()); // 线程安全
+		Set<String> set = new CopyOnWriteArraySet<String>(); // 线程安全 读写分离 底层是CopyOnWriteArrayList
+		for (int i = 1; i < 100; i++) {
+			new Thread(() -> {
+				set.add(UUID.randomUUID().toString().substring(0, 8));
+				System.out.println(set);
+			}, "Thread:" + i).start();
+		}
+	}
 
-    private static void listNotSafe() {
-        List<String> list = new java.util.concurrent.CopyOnWriteArrayList<String>();
+	private static void listNotSafe() {
+		List<String> list = new java.util.concurrent.CopyOnWriteArrayList<String>();
 //        List<String> list = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            new Thread(()->{
-                list.add(UUID.randomUUID().toString().substring(0, 8));
-                System.out.println(list);
-            }).start();
+		for (int i = 0; i < 100; i++) {
+			new Thread(() -> {
+				list.add(UUID.randomUUID().toString().substring(0, 8));
+				System.out.println(list);
+			}).start();
 
-        }
-    }
+		}
+	}
 
 }
---
+++ src/main/java/juc/ReenterLockDemo.java
@@ -0,0 +1,79 @@
+package juc;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+/**
+ * @author wis_edu
+ * @version 1.0
+ * @date 2020年4月22日
+ */
+public class ReenterLockDemo {
+	public static void main(String[] args) throws Exception {
+
+		Phone phone = new Phone();
+
+		new Thread(() -> {
+			try {
+				phone.sendSMS();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}, "t1").start();
+
+		new Thread(() -> {
+			try {
+				phone.sendSMS();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}, "t2").start();
+
+		TimeUnit.SECONDS.sleep(2);
+
+		Thread t3 = new Thread(phone, "t2");
+		Thread t4 = new Thread(phone, "t4");
+		t4.start();
+		t3.start();
+	}
+}
+
+class Phone implements Runnable {
+	public synchronized void sendSMS() throws Exception {
+		System.out.println(Thread.currentThread().getName() + "\t invoked SendSMS()");
+		sendEMail();
+	}
+
+	public synchronized void sendEMail() throws Exception {
+		System.out.println(Thread.currentThread().getName() + "\t invoked sendEMail()=========");
+	}
+
+	Lock lock = new ReentrantLock();
+
+	@Override
+	public void run() {
+		get();
+	}
+
+	private void get() {
+		lock.lock();
+		try {
+			set();
+		} finally {
+			lock.unlock();
+		}
+
+	}
+
+	private void set() {
+		lock.lock();
+		try {
+			System.out.println(Thread.currentThread().getName() + "\t invoked set()(*&(&(&(*&(&(&(&(*");
+		} finally {
+			lock.unlock();
+		}
+
+	}
+}
